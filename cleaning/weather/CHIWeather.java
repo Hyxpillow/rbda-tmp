f92ffb5c4dbd0b4cfe2f516c54ffd021e25b405b
Hyxpillow <hyxpillow@outlook.com>
2024-11-22 00:44:13 -0500

    x

--- cleaning/weather/CHIWeather.java
+++ cleaning/weather/CHIWeather.java
@@ -2,6 +2,7 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 public class CHIWeather {
