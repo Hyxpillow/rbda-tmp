de79682deb0b09e0c2bec491283e338117e7f4b9
Hyxpillow <hyxpillow@outlook.com>
2024-12-06 14:38:10 -0500

    bugs fixed

--- cleaning/CHICrashJoinWeather.java
+++ cleaning/CHICrashJoinWeather.java
@@ -22,7 +22,10 @@ public class CHICrashJoinWeather {
         jobCrashCleaning.setMapperClass(CHICrashMapper.class);
         jobCrashCleaning.setCombinerClass(CHICrashReducer.class);
         jobCrashCleaning.setReducerClass(CHICrashReducer.class);
-
+        jobWeatherCleaning.setMapOutputKeyClass(Text.class);
+        jobWeatherCleaning.setMapOutputValueClass(IntWritable.class);
+        jobWeatherCleaning.setReduceOutputKeyClass(NullWritable.class);
+        jobWeatherCleaning.setReduceOutputValueClass(Text.class);
 	    jobCrashCleaning.setNumReduceTasks(1);
         jobCrashCleaning.waitForCompletion(true);
 
