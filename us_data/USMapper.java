b455feec0408cea56e7c0854b78e1acb71ab2a16
Hyxpillow <hyxpillow@outlook.com>
2024-12-09 14:25:35 -0500

    bugs fixed

--- us_data/USMapper.java
+++ us_data/USMapper.java
@@ -37,7 +37,7 @@ public class USMapper
 
         String conditions = splitLine.get(28);
         String rainOrNot;
-        if (conditions.contains("Rain") || conditions.contains("Snow")) {
+        if (!conditions.contains("Clear")) {
             rainOrNot = "1";
         } else {
             rainOrNot = "0";
