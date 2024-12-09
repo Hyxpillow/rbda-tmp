cc642b5eab2544512e65f080c7182d2e0dda9170
Hyxpillow <hyxpillow@outlook.com>
2024-12-09 14:57:35 -0500

    bugs fixed

--- chicago_data/run.sh
+++ chicago_data/run.sh
@@ -11,7 +11,7 @@ rm -f *.class *.jar
 
 javac -classpath `hadoop classpath` *.java
 jar cvf data.jar *.class
-hadoop jar data.jar $MAIN_CLASS $INPUT_FILE $OUTPUT_DIR
+hadoop jar data.jar $MAIN_CLASS $INPUT_FILE1 $INPUT_FILE2 $OUTPUT_DIR
 
 hadoop fs -rm -f $HIVE_TABLE_DIR/$HIVE_TABLE_FILE
 hadoop fs -mv $OUTPUT_DIR/part-r-00000 $HIVE_TABLE_DIR/$HIVE_TABLE_FILE
